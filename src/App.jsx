dfecef42a3b6db621934b3240cb505a993ec7086
afolabi <afolabiadesola78@gmail.com>
2025-10-09 10:13:20 -0700

    created headline cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Food from "./components/Food"
+import HeadlineCards from "./components/HeadlineCards"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -9,6 +11,8 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards />
+      <Food />
     </div>
   )
 }
---
+++ src/components/Food.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Food = () => {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Food
